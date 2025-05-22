b7c3fa6dc71e0586d84f4252696c82f42936d6e4
Mike Haggerty <haggertymp@gmail.com>
2025-05-22 11:53:35 -0400

    Update consts.ts

--- website/src/consts.ts
+++ website/src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Praxio';
-export const SITE_DESCRIPTION = 'The Intelligent Way to Build Better Habits';
+export const SITE_DESCRIPTION = 'Hack your Habits';
